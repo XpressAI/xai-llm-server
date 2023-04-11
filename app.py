d3c5da804392430d9c530631f21b8f9c5ce1ab6a
MFA-X-AI <fahreza@xpress.ai>
2023-04-11 18:38:52 +0900

    fix model excess output

--- app.py
+++ app.py
@@ -5,20 +5,33 @@ app = Flask(__name__)
 from rwkvstic.load import RWKV
 from rwkvstic.agnostic.backends import TORCH
 import torch
+import os
 
 useGPU = False # False
-path = ""
 
-model = RWKV(path, mode=TORCH, useGPU=useGPU, dtype=torch.bfloat16)
+model_path = os.environ['model_path']
+model = RWKV(model_path, mode=TORCH, useGPU=useGPU, dtype=torch.bfloat16)
+
+def generate_answer(user_input):
+    number = 100
+    stopStrings = ["\n\n"]
+    stopTokens = [0]
+    temp = 1
+
+    def progressLambda(properties):
+        print("progress:", properties["progress"] / properties["total"])
+
+    model.loadContext(newctx=f"Q: {user_input}\n\nA:")
+    output = model.forward(number=number, stopStrings=stopStrings, stopTokens=stopTokens, temp=temp, progressLambda=progressLambda)
+
+    return output["output"]
 
 @app.route('/completions', methods=['POST'])
 def home():
     data = request.get_json()
     user_input = data.get('user_input', "")
-    user_prompt = f"Q: {user_input}\n\nA:"
-    model.loadContext(newctx=user_prompt)
-    output = model.forward(number=100)["output"]
-    return jsonify(answer=output)
-
+    answer = generate_answer(user_input)
+    return jsonify(answer=answer)
+    
 if __name__ == '__main__':
     app.run(debug=True)
